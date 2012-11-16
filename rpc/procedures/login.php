07db5855c241f52000b3b9ee7984fb798e54fc9c
karlsve <admin@karlsve.de>
2012-11-16 12:46:47 +0100

    login fixed

--- rpc/procedures/login.php
+++ rpc/procedures/login.php
@@ -26,6 +26,7 @@ function rpc_login($xml, $result, $args) {
 	$login = lima_login($args->username, $args->password);
 	// error code
 	$output = 'true';
+	$error = 'unknown'
 	if($login === false)
 		$output = 'false';
 	if($login === 0)
