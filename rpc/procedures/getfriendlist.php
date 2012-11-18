30bec15f660865e50b194ffd38f5e7179ac9fe73
karlsve <admin@karlsve.de>
2012-11-18 09:55:27 +0100

    getfriendlist.php added to show the friendlist as accessible through almost any part of lima-city in the right column

---
+++ rpc/procedures/getfriendlist.php
@@ -0,0 +1,44 @@
+<?php
+
+function rpc_getFriendListe($xml, $result, $args) {
+	global $url_homepage;
+	$doc = phpQuery::newDocument(get_request_cookie($url_homepage, "sid={$args->sid}"));
+	addToCache($url_homepage, $doc, "sid={$args->sid}");
+	if(!lima_checklogin($xml, $result, $args->sid))
+		return $result;
+	$onlineelements = $doc->find('a:has(img[alt=Online])');
+	$onlinenum = $onlineelements->length();
+	$onlinexml = $xml->createElement('online');
+	$onlinexml->appendChild($xml->createElement('number', $onlinenum));
+	foreach($onlineelements as $onlineelement)
+	{
+		$onlineelement = pq($onlineelement);
+		$onlineelement->remove('img');
+		$user = trim($onlineelement->text());
+		$link = $onlineelement->attr('href');
+		$userxml = $xml->createElement('user');
+		$userxml->appendChild($xml->createElement('name', $user));
+		$userxml->appendChild($xml->createElement('link', $link));
+		$onlinexml->appendChild($userxml);
+	}
+	$result->appendChild($onlinexml);
+	$offlineelements = $doc->find('a:has(img[alt=Offline])');
+	$offlinenum = $offlineelements->length();
+	$offlinexml = $xml->createElement('offline');
+	$offlinexml->appendChild($xml->createElement('number', $offlinenum));
+	foreach($offlineelements as $offlineelement)
+	{
+		$offlineelement = pq($offlineelement);
+		$offlineelement->remove('img');
+		$user = trim($offlineelement->text());
+		$link = $offlineelement->attr('href');
+		$userxml = $xml->createElement('user');
+		$userxml->appendChild($xml->createElement('name', $user));
+		$userxml->appendChild($xml->createElement('link', $link));
+		$offlinexml->appendChild($userxml);
+	}
+	$result->appendChild($offlinexml);
+	return $result;
+}
+
+xmlrpc_register_function('getFriendList', array('sid'), 'rpc_getFriendListe');
