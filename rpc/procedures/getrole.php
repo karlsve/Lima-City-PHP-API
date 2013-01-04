ed1c68b09d5da67b9375348f47a80baeb636b5da
hackyourlife <root@hackyourlife>
2013-01-04 21:25:20 +0100

    getRole corrected

--- rpc/procedures/getrole.php
+++ rpc/procedures/getrole.php
@@ -2,16 +2,10 @@
 
 function rpc_getRole($xml, $result, $args) {
 	global $url_homepage;
-	$doc = get_cached_any_cookie($url_homepage, "sid={$args->cookie}");
+	$doc = get_cached_any_cookie($url_homepage, "sid={$args->sid}");
 	$node = $doc->find('h3.user + ul li a[href="/usercp/page%3Agulden"] img')->get(0);
-	$result->appendChild($xml->createTextNode($node->attributes->getNamedItem('alt')->nodeValue));
+	$result->appendChild($xml->createTextNode(pq($node)->attr('alt')));
 	return $result;
 }
 
-xmlrpc_register_function(
-	'getRole',
-	array(
-		'sid'
-	),
-	'rpc_getRole'
-);
+xmlrpc_register_function('getRole', array('sid'), 'rpc_getRole');
