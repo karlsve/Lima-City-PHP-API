94cefd4c7dcb259f788f143c547bea1d2a3474a5
hackyourlife <root@hackyourlife.tk>
2013-06-14 21:26:17 +0200

    getPostThread fixed

--- rpc/common/curl.php
+++ rpc/common/curl.php
@@ -120,11 +120,22 @@ function get_location($url) {
 	curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER , true);
 	curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1');
-	curl_setopt($curl, CURLOPT_HEADER, false);
+	curl_setopt($curl, CURLOPT_HEADER, true);
 	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-	curl_exec($curl);
-	$location = curl_getinfo($curl, CURLINFO_EFFECTIVE_URL);
+	$data = curl_exec($curl);
+	//$location = curl_getinfo($curl, CURLINFO_EFFECTIVE_URL);
+	// split header and contents
+	$endofheader = strpos($data, "\r\n\r\n");
+	$size = 4;
+	if($endofheader === false) {
+		$endofheader = strpos($data, "\n\n");
+		$size = 2;
+	}
+	$header = substr($data, 0, $endofheader);
+	$location = $url;
+	if(preg_match('|location:\s+(.*)|i', $header, $match))
+		$location = $match[1];
 	curl_close($curl);
 	return($location);
 }
--- rpc/procedures/getpostthread.php
+++ rpc/procedures/getpostthread.php
@@ -2,13 +2,13 @@
 
 function rpc_getPostThread($xml, $result, $args) {
 	global $url_board;
-	global $url_thread;
+	global $url_origin;
 	$location = get_location("$url_board/action:jump/{$args->id}");
-	preg_match("|$url_thread/(.+)/page%3A([0-9]+)/perpage%3A([0-9]+)#[0-9]+$|", $location, $match);
+	preg_match("|/thread/(.+)/page%3A([0-9]+)/perpage%3A([0-9]+)|", $location, $match);
 	$name = $match[1];
 	$page = $match[2];
 	$perpage = $match[3];
-	$result->appendChild($xml->createElement('location', $location));
+	$result->appendChild($xml->createElement('location', $url_origin . $location));
 	$result->appendChild($xml->createElement('name', $name));
 	$result->appendChild($xml->createElement('page', $page));
 	$result->appendChild($xml->createElement('perpage', $perpage));
