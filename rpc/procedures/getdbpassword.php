b84847559cc121792a0f1bfc28f3ffda0c702034
hackyourlife <root@hackyourlife.tk>
2013-06-15 12:25:11 +0200

    getDBPassword and getFTPPassword fixed

--- rpc/procedures/getdbpassword.php
+++ rpc/procedures/getdbpassword.php
@@ -3,25 +3,13 @@
 function rpc_getDBPassword($xml, $result, $args) {
 	global $url_databases;
 
-	$response = get_request_raw($url_databases, "auth_token_session={$args->sid}");
-
-	$pattern_sid = '|Set-Cookie: sid=(.*?); path=/|i';
-	if(!preg_match($pattern_sid, $response['header'], $match))
-		return false;
-
-	$sid = $match[1];
-
-	$doc = phpQuery::newDocument($response['content']);
+	$doc = phpQuery::newDocument(get_request_cookie($url_databases, "auth_token_session={$args->sid}"));
 	addToCache($url_databases, $doc, "auth_token_session={$args->sid}");
 	if(!lima_checklogin($xml, $result, $args->sid))
 		return $result;
 
-	$code = $doc->find('div.content table input[name="code"]')->attr('value');
-	$meet = $doc->find('div.content table input[name="meet"]')->attr('value');
-
-	$doc = phpQuery::newDocument(post_request_cookie("$url_databases/action%3Ashow", "code=$code&meet=$meet", "auth_token_session={$args->sid};sid=$sid"));
-	$username = $doc->find('div.content table tr:has(td strong:contains("Benutzername:")) td:nth-child(2) pre')->text();
-	$password = $doc->find('div.content table tr:has(td strong:contains("Kennwort:")) td:nth-child(2) pre')->text();
+	$username = pq($doc->find('dl.well dd code')->get(2))->text();
+	$password = $doc->find('dl.well dd #database_password')->text();
 	$result->appendChild($xml->createElement('username', $username));
 	$result->appendChild($xml->createElement('password', $password));
 	return $result;
--- rpc/procedures/getftppassword.php
+++ rpc/procedures/getftppassword.php
@@ -3,29 +3,12 @@
 function rpc_getFTPPassword($xml, $result, $args) {
 	global $url_ftp;
 
-	$response = get_request_raw($url_ftp, "auth_token_session={$args->sid}");
-
-	$pattern_sid = '|Set-Cookie: sid=(.*?); path=/|i';
-	if(!preg_match($pattern_sid, $response['header'], $match))
-		return false;
-
-	$sid = $match[1];
-
-	$doc = phpQuery::newDocument($response['content']);
+	$doc = phpQuery::newDocument(get_request_cookie($url_ftp, "auth_token_session={$args->sid}"));
 	addToCache($url_ftp, $doc, "auth_token_session={$args->sid}");
 	if(!lima_checklogin($xml, $result, $args->sid))
 		return $result;
 
-	$code = $doc->find('div.content table input[name="code"]')->attr('value');
-	$meet = $doc->find('div.content table input[name="meet"]')->attr('value');
-
-	if(empty($code) && empty($meet)) {
-		$result->appendChild($xml->createElement('error'));
-		return $result;
-	}
-
-	$doc = phpQuery::newDocument(post_request_cookie("$url_ftp/action%3Ashow", "code=$code&meet=$meet", "auth_token_session={$args->sid};sid=$sid"));
-	$password = $doc->find('div.content table tr:has(td strong:contains("Kennwort:")) td:nth-child(2) pre')->text();
+	$password = $doc->find('#ftp_password')->text();
 	$result->appendChild($xml->createElement('password', $password));
 	return $result;
 }
