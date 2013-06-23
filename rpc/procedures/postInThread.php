83c96a253f4fa631588e3489889f9da104012673
hackyourlife <root@hackyourlife.tk>
2013-06-23 21:23:41 +0200

    postInThread finally fixed

--- rpc/procedures/postInThread.php
+++ rpc/procedures/postInThread.php
@@ -4,8 +4,15 @@ function rpc_postInThread($xml, $result, $args) {
 	global $url_origin;
 	global $url_thread;
 
-	$doc = phpQuery::newDocument(get_request_cookie("$url_thread/{$args->thread}/action%3Apost", "auth_token_session={$args->sid}"));
-	addToCache("$url_thread/$thread/action%3Apost", $doc, "auth_token_session={$args->sid}");
+	$response = get_request_raw("$url_thread/{$args->thread}/action%3Apost", "auth_token_session={$args->sid}");
+	$pattern_sid = '|Set-Cookie: sid=(.*?); path=/|i';
+	if(!preg_match($pattern_sid, $response['header'], $match)) {
+		$result->appendChild($xml->createElement('response', 'error'));
+		return $result;
+	}
+	$sid = $match[1];
+	$doc = phpQuery::newDocument($response['content']);
+	addToCache("$url_thread/{$args->thread}/action%3Apost", $doc, "auth_token_session={$args->sid}");
 	if(!lima_checklogin($xml, $result, $args->sid))
 		return $result;
 
@@ -18,6 +25,7 @@ function rpc_postInThread($xml, $result, $args) {
 		'id	'	=> $threadid,
 		'count'		=> '1'
 	);
+
 	$response = post_request_cookie("$url_origin/ajax_replyeditor", $requestdata, "auth_token_session={$args->sid}");
 	if($response > 0) {
 		// FIXXME:
@@ -35,7 +43,7 @@ function rpc_postInThread($xml, $result, $args) {
 		'count'		=> '1'
 	);
 
-	$response = post_request_cookie("$url_thread/{$args->thread}/action%3Apost", $requestdata, "auth_token_session={$args->sid}");
+	$response = post_request_cookie("$url_thread/{$args->thread}/action%3Apost", $requestdata, "auth_token_session={$args->sid};sid=$sid");
 	$result->appendChild($xml->createElement('response', 'ok'));
 
 	return $result;
